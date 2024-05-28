decb287b4b65781fc958fc84b34a52dc896def75
Santosh Dahal <sirishsds@gmail.com>
2024-05-28 13:36:41 +0545

    Datatypes Summary

---
+++ 01_Basics/dataTypesSummary.js
@@ -0,0 +1,48 @@
+/* 
+##Primitive Data Types
+7 types:
+1.String
+2.Number
+3.Boolean
+4.null
+5.undefined
+6.Symbol
+7.BigInt
+
+##Refrence
+1.Array
+2.Objects
+3.Functions
+
+*/
+
+let score = 100;
+let marks = 10.5;
+const isOnline = true;
+let  userEmail;
+
+//To Declare symbol
+const id = Symbol('123');
+const bigNumber = 5230889633211456988536988775258652n;
+
+//Array
+
+const languages =["Java","c","c++","Python"];
+
+//Declare a Object 
+
+let stuentInfo{
+
+    name=santosh,
+    age=20,
+    facualty=IT
+}
+
+//Functions
+
+const myFunction = function () {
+    console.log("Hello World!");
+}
+
+
+
