80d379585797ea91f87667dcdb4d995187309ec6
Santosh Dahal <sirishsds@gmail.com>
2024-05-28 11:27:55 +0545

    Variables

---
+++ 01_Basics/01_variable.js
@@ -0,0 +1,9 @@
+const accountId=123364789
+let accountEmail= "santosh@google.com"
+var accountPassword="789654"
+accountCity="Kathmandu"
+
+//accountId=5 const value cant be change
+
+console.log(accountId)
+console.table([accountEmail,accountPassword,accountCity])//it is use to print variable in table format
