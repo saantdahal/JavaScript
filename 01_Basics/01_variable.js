1d19db1f634ae3f75565a165132523964b5937b5
Santosh Dahal <sirishsds@gmail.com>
2024-05-28 11:55:56 +0545

    DataTypes

--- 01_Basics/01_variable.js
+++ 01_Basics/01_variable.js
@@ -1,9 +1,10 @@
-const accountId=123364789
-let accountEmail= "santosh@google.com"
-var accountPassword="789654"
-accountCity="Kathmandu"
+const accountId = 123364789;
+let accountEmail = "santosh@google.com";
+var accountPassword = "789654";
+accountCity = "Kathmandu";
+let accountState;
 
 //accountId=5 const value cant be change
 
-console.log(accountId)
-console.table([accountEmail,accountPassword,accountCity])//it is use to print variable in table format
+console.log(accountId);
+console.table([accountEmail, accountPassword, accountCity, accountState]); //it is use to print variable in table format
---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict";
+let name ="santosh";
+let age = 20;
+let isOnline=false;
+
+
+// Data Types in JavaScript
+
+//number
+//bigInt
+//string
+//boolean
+//null
+//undefined
+//symbol -> Unique
+ 
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
