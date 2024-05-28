2d78d28c8724134776907175d2578365b2674645
Santosh Dahal <sirishsds@gmail.com>
2024-05-28 12:46:28 +0545

    Operators

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -1,16 +1,37 @@
 let score = "33abc"
 
-console.log(typeof score);
+// console.log(typeof score);
 
 let valueInNumber= Number (score);
 
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 
 // "33" -> 33
 //"33abc" -> NaN
 // true = 1, false=0
+
+
+// *******************************Operations*********************************
+let value = 3;
+
+let negValue=-value;
+// console.log(negValue);
+
+
+console.log("3"+2+1);
+console.log(3+'2'+'1');
+console.log(3+2+'1');
+
+console.log(3*2/5%6);//Avoid using this types of codes
+
+let gameCounter=100;
+// gameCounter++;
+console.log(gameCounter);
+
+++gameCounter;
+console.log(gameCounter);
 
 
 
