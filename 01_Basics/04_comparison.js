fe98438f4c2dedaaa3fb5382985f28f35e30a2de
Santosh Dahal <sirishsds@gmail.com>
2024-05-28 13:01:11 +0545

    COmparision

---
+++ 01_Basics/04_comparison.js
@@ -0,0 +1,11 @@
+
+// console.log(null > 0);
+// console.log(null < 0);
+// console.log(null == 0);
+// console.log(0 >= null);
+
+
+//if we check 'undefinied' in the above compasion it return false
+
+console.log("2"> 1);
+console.log(2 < "1");
