d04e1637f599cf0c3df1b56c3455e16be719d7da
Santosh Dahal <sirishsds@gmail.com>
2024-06-05 07:39:30 +0545

    Date and time

---
+++ 01_Basics/07_dateInjs.js
@@ -0,0 +1,15 @@
+//Dates
+
+let myDate = new Date();
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toLocaleString());
+
+//Months are starts with 0 in this formatjs
+let myCreateDate = new Date(2024, 5, 24);
+console.log(myCreateDate.toDateString());
+
+//Another format (like :  DD-MM-YY)
+ myCreateDate = new Date("06-05-2024");
+console.log(myCreateDate.toString());
