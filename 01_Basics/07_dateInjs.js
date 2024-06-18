f368bcd5a58990f217b86e3ef14b8c281673a780
santoshdahal <about@santoshdahal.info.np>
2024-06-18 20:12:18 +0545

    Test

--- 01_Basics/07_dateInjs.js
+++ 01_Basics/07_dateInjs.js
@@ -9,7 +9,6 @@ console.log(myDate.toLocaleString());
 //Months are starts with 0 in this formatjs
 let myCreateDate = new Date(2024, 5, 24);
 console.log(myCreateDate.toDateString());
-
 //Another format (like :  DD-MM-YY)
  myCreateDate = new Date("06-05-2024");
 console.log(myCreateDate.toString());
